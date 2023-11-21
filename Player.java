85269b1152fea095e4b001799fdfb62b17cb76cb
imad <147118339+eddineimad@users.noreply.github.com>
2023-11-21 02:55:34 +0100

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,21 @@
+public class Player {
+    String name;
+    int age ;
+    String  post ;
+    String club ;
+    String  nationality ;
+
+    void pass(){
+        System.out.println(name + " Passing !") ;
+    }
+
+    void shoot(){
+        System.out.println(name + " Shooting !") ;
+    }
+
+    void jump(){
+        System.out.println(name + " Jumping !") ;
+    }
+
+    
+}
