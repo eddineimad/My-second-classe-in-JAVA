b16f0dc2db86f079c4ca45452cd2acc51374a852
imad <147118339+eddineimad@users.noreply.github.com>
2023-11-21 02:54:04 +0100

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Player pla1 = new Player();
+        pla1.name = "Erling Halland" ;
+        pla1.age = 23 ;
+        pla1.post = "ST" ;
+        pla1.club = "Manchester City" ;
+        pla1.nationality = "Norway" ;
+        pla1.pass() ;
+        pla1.shoot() ;
+        pla1.jump() ;
+    }
+}
